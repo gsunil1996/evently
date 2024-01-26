039c832e6d758274e37fa63136813acea259adab
gsunil1996 <gajulasunilkumar123@gmail.com>
2024-01-26 10:41:15 +0530

    update

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+
+const Home = () => {
+  return (
+    <main>
+      <h1 className="text-4xl">Evently</h1>
+      <Button variant="destructive" className="px-10">Hello</Button>
+    </main>
+  )
+}
+
+export default Home
